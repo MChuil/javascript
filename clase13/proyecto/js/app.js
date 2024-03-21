e77995f19015366d5810aae49d29f6a846280f9f
Miguel Chuil <developer.chuil@gmail.com>
2024-03-20 20:43:33 -0600

    Parte final del carrito

--- clase13/proyecto/js/app.js
+++ clase13/proyecto/js/app.js
@@ -1,5 +1,7 @@
 const listaProductos = document.querySelector('.products-slick')
 listaProductos.addEventListener('click', agregarProducto)
+const clearCart = document.querySelector('#clearCart')
+clearCart.addEventListener('click', deleteCart )
 let articleCar = []
 
 
@@ -24,16 +26,31 @@ function leerProducto(producto){
         cantidad : 1
     }
 
-    //agregar el producto al arreglo del carrito
-    articleCar.push(infoProduct)
-    console.log(articleCar)
+    //Actualizar la cantidad en productos existentes
+    //1. verificar que exista
+    const exist = articleCar.some(product => product.id == id)
+    if(exist){ //si existe actualizamos la cantidad
+        //buscar el producto y aumentar la cantidad
+        const newArticleCar = articleCar.map( product =>{
+            if(product.id == id){ //si es igual el Id
+                product.cantidad++
+                return product
+            }else{  //si no es igual el id
+                return product
+            }
+        })
+        articleCar = newArticleCar
+    }else{ //si no existe agregamos el producto al array
+        //agregar el producto al arreglo del carrito
+        articleCar.push(infoProduct)
+    }
     cartHtml()
 }
 
 
 //mostrar el carrito en el HTML
-
 function cartHtml(){
+    console.log(articleCar)
     const cartList = document.querySelector('.cart-list')
     cartList.textContent = ''
     let subtotal = 0
@@ -61,17 +78,16 @@ function cartHtml(){
         //crear y agregar el h4
         const h4 = document.createElement('h4')
         h4.className ='product-price'
-        const span = document.createElement('span')
-        span.className='qty'
-        span.textContent= item.cantidad
-        h4.appendChild(span)
-        h4.textContent= `$ ${Number(item.precio).toFixed(2)}`
+        h4.innerHTML = `<span class="qty">${item.cantidad}x</span> ${Number(item.precio).toFixed(2)}`
         divBody.appendChild(h4)
         divPadre.appendChild(divBody)
         let importe = Number(item.cantidad) * Number(item.precio)
+        
         //crear boton
         const btn = document.createElement('button')
         btn.className ='delete'
+        btn.setAttribute('data-id', item.id)
+        btn.onclick= () => deleteProduct(item.id)
         const i = document.createElement('i')
         i.classList='fa fa-close'
         btn.append(i)
@@ -92,4 +108,30 @@ function cartHtml(){
     cartSummary.appendChild(small)
     cartSummary.appendChild(h5)
 
+    const qty = document.querySelectorAll('.qty')
+    qty[1].textContent = articleCar.length
+}
+
+function deleteCart(){
+    const cartList = document.querySelector('.cart-list')
+    cartList.textContent = ''
+
+    const small = document.createElement('small')
+    small.textContent = `0 Productos seleccionados`
+    const h5 = document.createElement('h5')
+    h5.textContent = `SUBTOTAL: $ 0`
+
+    const cartSummary = document.querySelector('.cart-summary')
+    cartSummary.textContent = ''
+    cartSummary.appendChild(small)
+    cartSummary.appendChild(h5)
+}
+
+function deleteProduct(id){
+
+    const newArticleCar = articleCar.filter(product => product.id != id)
+    
+    console.table(newArticleCar)
+    articleCar = newArticleCar
+    cartHtml()
 }
